27eaaf8231422e68e857d13f6b34f55c14225b78
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-07 07:12:46 +0530

    chore: define routing, pages and app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Accounts from "./pages/Accounts";
+import Users from "./pages/Users";
+import Feedback from "./pages/Feedback";
+import Settings from "./pages/Settings";
+import AppLayout from "./ui/AppLayout";
+
 function App() {
-  return <div className="text-4xl font-black">Hello Pai</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate to="/Dashboard" replace={true} />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="accounts" element={<Accounts />} />
+          <Route path="users" element={<Users />} />
+          <Route path="feedback" element={<Feedback />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
