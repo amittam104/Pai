b1a2a9fe0f42d708f51f557782e73be56f5d3f55
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-17 12:49:03 +0530

    chore: update header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 function Header() {
   return (
-    <header className="bg-white border-b border-slate-200 row-span-1 col-span-5">
-      Header
+    <header className="bg-white border-b border-slate-200 row-span-1 col-span-6">
+      <div className="flex items-center justify-between py-3 px-6">
+        <p className="font-semibold text-lg">Welcome back</p>
+        <div className="flex items-center gap-4">
+          <Button variant="ghost">Log in</Button>
+          <Button>Sign up</Button>
+        </div>
+      </div>
     </header>
   );
 }
